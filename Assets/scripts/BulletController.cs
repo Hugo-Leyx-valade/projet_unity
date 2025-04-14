b1770fb5de8454c90905a2c62e412a9205a9e2a5
hugol <hugo.leyx-valade@efrei.net>
2025-04-14 19:23:12 +0200

    jiji

--- Assets/scripts/BulletController.cs
+++ Assets/scripts/BulletController.cs
@@ -1,17 +1,27 @@
 using UnityEngine;
+using UnityEngine.InputSystem.iOS;
+using UnityEngine.Windows.Speech;
 
 public class BulletController : MonoBehaviour
 {
-    public float speed;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public float speed; 
     void Start()
     {
-        
+        Destroy(this.gameObject, 2.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.position+=Vector3.forward*speed*Time.deltaTime;
+        this.transform.position += Vector3.forward * speed * Time.deltaTime;
+    }
+
+    private void OnColisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "ennemy"){
+            Destroy(this.gameObject);
+            Destroy(collision.gameObject);
+        }
     }
 }
