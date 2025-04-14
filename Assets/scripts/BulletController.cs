7476c8a714d6977d343b2d4dc514746e740fc340
Hugo-Leyx-valade <hugo.leyx-valade@efrei.net>
2025-04-14 16:48:29 +0200

    dzqdqzd

--- Assets/scripts/BulletController.cs
+++ Assets/scripts/BulletController.cs
@@ -4,12 +4,8 @@ using UnityEngine.Windows.Speech;
 
 public class BulletController : MonoBehaviour
 {
-<<<<<<< HEAD
+
     public float speed; 
-=======
-    public float speed;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
->>>>>>> 42b59b420b7dc1643d851c0c1c2520f39e83ec39
     void Start()
     {
         Destroy(this.gameObject, 2.5f);
@@ -18,7 +14,6 @@ public class BulletController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-<<<<<<< HEAD
         this.transform.position += Vector3.forward * speed * Time.deltaTime;
     }
 
@@ -28,9 +23,5 @@ public class BulletController : MonoBehaviour
             Destroy(this.gameObject);
             Destroy(collision.gameObject);
         }
-=======
-        this.transform.position+=Vector3.forward*speed*Time.deltaTime;
->>>>>>> 42b59b420b7dc1643d851c0c1c2520f39e83ec39
     }
 }
-
